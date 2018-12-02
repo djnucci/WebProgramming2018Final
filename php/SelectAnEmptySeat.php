71a10f7cbce7c9e76933367ef634566a153065fc
Avdon Racki <avdon@racki.ca>
2018-12-02 15:22:46 -0500

    indented

--- php/SelectAnEmptySeat.php
+++ php/SelectAnEmptySeat.php
@@ -10,33 +10,33 @@
 		</form>
 		<form method="post">
 			<?php
-			require_once "../config.php";
-			$table = "movies";
+				require_once "../config.php";
+				$table = "movies";
 
-			//collect results from movieselector dropdown
-			$movie = $_POST["movie"];
+				//collect results from movieselector dropdown
+				$movie = $_POST["movie"];
 
-			//builds dropdown of available seats for a given movie
-			echo "<select name='seatselector'>";
-				$seatrequest = "SELECT seat_ID, bookState FROM ".$table." WHERE bookState='free' AND movie='".$movie."'";
+				//builds dropdown of available seats for a given movie
+				echo "<select name='seatselector'>";
+					$seatrequest = "SELECT seat_ID, bookState FROM ".$table." WHERE bookState='free' AND movie='".$movie."'";
 
-				if($result = mysqli_query($conn, $seatrequest)){
-					if(mysqli_num_rows($result) > 0){
-						//create dropdown entry for each SQL table row
-						while($row = mysqli_fetch_array($result)){
-							echo "<option value='".$row["seat_ID"]."'>"." Seat ".$row["seat_ID"]."</option>";}
-						// Free result set
-						mysqli_free_result($result);
+					if($result = mysqli_query($conn, $seatrequest)){
+						if(mysqli_num_rows($result) > 0){
+							//create dropdown entry for each SQL table row
+							while($row = mysqli_fetch_array($result)){
+								echo "<option value='".$row["seat_ID"]."'>"." Seat ".$row["seat_ID"]."</option>";}
+							// Free result set
+							mysqli_free_result($result);
+						} else{
+							echo "No records matching your query";}
 					} else{
-						echo "No records matching your query";}
-				} else{
-				echo "ERROR: Could not execute " . mysqli_error($conn);}
-				// Close connection
-			echo "</select>";
-			echo "<input type='submit' value='Book Seats'>";
+					echo "ERROR: Could not execute " . mysqli_error($conn);}
+					// Close connection
+				echo "</select>";
+				echo "<input type='submit' value='Book Seats'>";
 
-			//close connection
-			mysqli_close($conn);
+				//close connection
+				mysqli_close($conn);
 			?>
 		</form>
 
