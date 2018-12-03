3447e3ae61a02594ef90e94aa463ebc9927a14b4
Avdon Racki <avdon@racki.ca>
2018-12-03 05:26:07 -0500

    get and set

--- php/GetMovieStatus.php
+++ php/GetMovieStatus.php
@@ -1,17 +1,54 @@
 <?php
-	$table = "movies";
+	//$movieName = $_POST["Movies"];
+	$movieName = '\'70 Jump Street: Dental Hygiene School\'';
+	$timeA = $timeB = $timeC = "";
 
-	include "../config.php";
+	include "config.php";
 
-	$sql = mysqli_query($conn,"SELECT * FROM movies");
-    $rows = array();
+	function queryMaker($movieName) {
+		$queryArray = array();
+			for ($i = 0; $i < 3; $i++){
+				if ($i == 0){
+					$command = "SELECT start_time FROM movies WHERE movie_name = $movieName AND room_ID = 'A'";
+					array_push($queryArray,$command);
+				} else if ($i == 1){
+					$command = "SELECT start_time FROM movies WHERE movie_name = $movieName AND room_ID = 'B'";
+					array_push($queryArray,$command);
+				} else {
+					$command = "SELECT start_time FROM movies WHERE movie_name = $movieName AND room_ID = 'C'";
+					array_push($queryArray,$command);
+				}
+			}
+		return $queryArray;
+	}
 
-    if(!($sql === FALSE)) {
-        while($row = mysql_fetch_array($sql)) {
-            $rows[] = $row;
-        }
-        echo json_encode($rows);
-    } else {
-        echo "{}";
-    }
+	$queryArray = queryMaker($movieName);
+
+	for ($i = 0; $i < 3; $i++){
+		$sql = mysqli_query($conn,$queryArray[$i]);
+		if(!($sql === FALSE)) {
+			$row = mysqli_fetch_array($sql);
+			if ($i == 0){
+				$timeA = $row[0];
+			} else if ($i == 1){
+				$timeB = $row[0];
+			} else{
+				$timeC = $row[0];
+			}
+		}
+	}
+
+	echo '<select name="times"><span id="chooseTime">';
+	echo '<option value="'.htmlspecialchars("null").'">'.htmlspecialchars("Please select a time...").'</option>';
+	for ($i = 0; $i < 3; $i++){
+		if ($i == 0){
+			echo '<option value="'.htmlspecialchars($i).'">'.htmlspecialchars($timeA).'</option>';
+		} else if ($i == 1){
+			echo '<option value="'.htmlspecialchars($i).'">'.htmlspecialchars($timeB).'</option>';
+		} else{
+			echo '<option value="'.htmlspecialchars($i).'">'.htmlspecialchars($timeC).'</option>';
+		}
+	}
+	echo '</span></select>';
+
 ?>
--- php/SetMovieStatus.php
+++ php/SetMovieStatus.php
@@ -4,6 +4,6 @@
     $querys = array("start_time", "end_time","movie_name","age_rating","room_ID","total_seats","taken_seats","row_a_seats","row_b_seats","row_c_seats","3D");
     $startTime = $endTime = $movieName = $ageRating = $roomID = $totalSeats = $takenSeats = $rowASeats = $rowBSeats = $rowCSeats = $is3D;
 
-    include "../config.php";
+    include "config.php";
 
 ?>
