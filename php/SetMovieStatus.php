c05b8434b1f4af1a9c0d2e8beb33c6c3c955630d
Avdon Racki <avdon@racki.ca>
2018-12-03 02:21:11 -0500

    updated get and set php files for movies

---
+++ php/SetMovieStatus.php
@@ -0,0 +1,9 @@
+<?php  
+    $table = "movies";
+
+    $querys = array("start_time", "end_time","movie_name","age_rating","room_ID","total_seats","taken_seats","row_a_seats","row_b_seats","row_c_seats","3D");
+    $startTime = $endTime = $movieName = $ageRating = $roomID = $totalSeats = $takenSeats = $rowASeats = $rowBSeats = $rowCSeats = $is3D;
+
+    include "../config.php";
+
+?>
